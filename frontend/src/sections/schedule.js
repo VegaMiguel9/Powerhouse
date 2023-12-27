f479359fb118c5f3001e95a6162295006cc2a594
Miguel Vega <vega@Miguels-MBP.lan>
2023-12-26 19:08:47 -0500

    added schedule page

--- frontend/src/sections/schedule.js
+++ frontend/src/sections/schedule.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/navBar/navBar";
+import Schedule from "../components/schedule/schedule";
 
 function schedule() {
   return (
     <div>
       <NavBar />
+      <Schedule />
     </div>
   );
 }
