a91559990c40f8a1712c9eb31f2d1d51389ba431
Ramon <vegaramon10@yahoo.com>
2023-12-26 20:14:03 -0500

    more work on meet the team

--- frontend/src/components/meetTheTeam/MeetTheTeam.js
+++ frontend/src/components/meetTheTeam/MeetTheTeam.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import "./MeetTheTeam.css"
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function MeetTheTeam() {
+
+    useEffect(() => {
+        AOS.init({ duration: "1000" });
+      });
+
   return (
     <div className='meetTheTeamLink'>
-       <section>
+       <section style={{paddingBottom: "5%"}}>
         <div>
             <img style={{width:"100%"}} src="https://static.wixstatic.com/media/7bd5ba_c547882ec1ed434c8db74705a1e321df~mv2.jpeg/v1/crop/x_24,y_917,w_5945,h_3083/fill/w_640,h_326,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/8269DCFC-CC39-4CFE-9822-FDCBF5EB76F3.jpeg" alt='coaching staff'/>
-            <h2 style={{color:"white", borderBottom:"#CAB08B 8px solid", margin:"0 5%" }}>OUR MISSION</h2>
+            <h2 className='ourMissionHeader' style={{color:"white", borderBottom:"#CAB08B 8px solid", margin:"0 5%" }}>OUR MISSION</h2>
         </div>
         <div>
-            <h2>TO CREATE AN ATMOSPHERE FULL OF <span>D.</span><span>R.</span><span>I.</span><span>V.</span><span>E.</span></h2>
+            <h2 data-aos="zoom-in" className='ourMissionHeader'>TO CREATE AN ATMOSPHERE FULL OF <span>D.</span><span>R.</span><span>I.</span><span>V.</span><span>E.</span></h2>
             <p><span>D</span>iversity. We welcome everyone and anyone</p>
             <p><span>R</span>espect. We value one another</p>
             <p><span>I</span>nclusivity. We provide equal opportunity to everyone who steps through our door</p>
@@ -19,9 +26,9 @@ export default function MeetTheTeam() {
         </div>
         <div>
             <img src='https://static.wixstatic.com/media/7bd5ba_431d27a66463433b92a8cb5aa0f0187a~mv2.jpg/v1/fill/w_640,h_488,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/7bd5ba_431d27a66463433b92a8cb5aa0f0187a~mv2.jpg' alt='coach'/>
-            <h2>Ramon Vega</h2>
+            <h2 data-aos="zoom-in" className='coachNameHeader'>Ramon Vega</h2>
             <h3>Owner & Coach</h3>
-            <p>My name is Ramon Vega. I started my journey in the fitness industry in 2013. 
+            <p style={{textAlign:"left"}}>My name is Ramon Vega. I started my journey in the fitness industry in 2013. 
             Much like many other trainers I started at a local corporate gym. 
             My continuous desire to grow my knowledge in health & fitness and to help others made me one of the most highly ranked trainers in the nation. 
             In 2017 I decided to venture out on my own and started my own Personal Training business. 
@@ -31,9 +38,9 @@ export default function MeetTheTeam() {
         </div>
         <div>
             <img src='https://static.wixstatic.com/media/7bd5ba_98a0c38c614b4cbd814d840dbb28a756~mv2.jpg/v1/fill/w_640,h_550,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/7bd5ba_98a0c38c614b4cbd814d840dbb28a756~mv2.jpg' alt='coach'/>
-            <h2>Cristina Vega</h2>
+            <h2 data-aos="zoom-in" className='coachNameHeader'>Cristina Vega</h2>
             <h3>Owner & Coach</h3>
-            <p>My name is Cristina Vega, and I am one of the founders of Powerhouse Fitness. 
+            <p style={{textAlign:"left"}}> My name is Cristina Vega, and I am one of the founders of Powerhouse Fitness. 
                 I specialize in weight loss and women’s fitness. 
                 I have experience with adolescent teens, women who are pregnant and postpartum. 
                 I have a great appreciation for a womens’ body and what it is capable of going through. 
@@ -46,24 +53,23 @@ export default function MeetTheTeam() {
         </div>
         <div>
             <img src='https://static.wixstatic.com/media/7bd5ba_bbc17685dec84592a0009fff85cd3c63~mv2.jpg/v1/fill/w_640,h_524,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/7bd5ba_bbc17685dec84592a0009fff85cd3c63~mv2.jpg' alt='coach'/>
-            <h2>David Ortiz</h2>
+            <h2 data-aos="zoom-in" className='coachNameHeader'>David Ortiz</h2>
             <h3>Coach & Personal Trainer</h3>
-            <p>My name is David Ortiz, I am an ACE certified personal trainer and specialize in Sports Performance development. 
+            <p style={{textAlign:"left"}}>My name is David Ortiz, I am an ACE certified personal trainer and specialize in Sports Performance development. 
                 I've had the opportunity to work with professional soccer coaches in Spain & Costa Rica. 
                 My goal is to help others reach their full athletic potential and provide them with the right tools for them to achieve their goals. 
             </p>
         </div>
-        <div>
+        <div  className='coachNameHeader'>
             <img src='https://static.wixstatic.com/media/7bd5ba_edadef4ce94645b49114143afe6e10d0~mv2.jpg/v1/fill/w_640,h_524,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/7bd5ba_edadef4ce94645b49114143afe6e10d0~mv2.jpg' alt='coach' />
-            <h2>Emanuel Martinez</h2>
+            <h2 data-aos="zoom-in">Emanuel Martinez</h2>
             <h3>Coach & Personal Trainer</h3>
-            <p>Hello! I’ve been into fitness for 7+ years and counting. 
+            <p style={{textAlign:"left"}}>Hello! I’ve been into fitness for 7+ years and counting. 
                 I enjoy almost any sport, outdoor or physical activities. 
                 One thing I’ve learned through all my life of “fitnessing,” is that character always comes first. 
             </p>
         </div>
        </section>
-      
     </div>
   )
 }
--- frontend/src/components/ourGym/OurGym.js
+++ frontend/src/components/ourGym/OurGym.js
@@ -5,13 +5,13 @@ export default function OurGym() {
     <div className='OurGymContainer' style={{backgroundColor: "white"}}>
         <section>
              <div>
-                <h2>Elite Strength & Conditioning Facility</h2>
+                <h2>ELITE STRENGTH & CONDITIONING FACILITY</h2>
                 <img style={{width: "100%"}} src='https://static.wixstatic.com/media/7bd5ba_5721f4be054d449685290e81297f0de0~mv2.jpeg/v1/crop/x_0,y_1083,w_6000,h_1835/fill/w_981,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/D6D996DC-4ED5-41F6-BAD7-5648D03B3F21.jpeg'  alt='training facility'/>
                 <p>When you walk into Powerhouse Fitness expect a positive and welcoming environment. Your goals will become ours and we promise to be in your corner throughout your fitness journey. Whether it's performing your first pull up or fitting into clothes you haven’t worn in a while, we’re here to prove you’re capable of amazing things. Lets celebrate your fitness victories together.</p>
              </div>
              <div className='container'>
                 <div>
-                    <h2>Be Challenged The Powerhouse Way</h2>
+                    <h2>BE CHALLENGED THE POWERHOUSE WAY</h2>
                     <video style={{width: "100%" }} preload= "none" playsInline = "true" controls src="https://video.wixstatic.com/video/7bd5ba_2efdad5a702e4983abd5141e6f4d89bd/1080p/mp4/file.mp4"></video>
                     <p>Our workout training plan is created in house by our head trainer with over a decade of fitness programming experience. We see what our members need help with in order to reach their full fitness potential and we design a plan that is best suited. Our goal is for you to walk out of the gym proud of the hard work you’ve put in and ready to take on whatever else you have planned for the day.</p>
                 </div>
